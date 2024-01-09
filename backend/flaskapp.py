2cc99094c718faae29864db0997624506ffdc57d
Linnkid_CHEN <56332312+LinnkidChen@users.noreply.github.com>
2024-01-09 11:41:30 +0800

    change

---
+++ backend/flaskapp.py
@@ -0,0 +1,24 @@
+# please set environment variable FLASK_APP=findGourmet.py
+
+import os
+import click
+from flask_migrate import Migrate
+from app import create_app, db
+
+# from app.models import User, Follow, Role, Permission, Post, Comment
+# from app.models import User, Role, Permission, FindG, PleEat, Success, FeeSummary
+from flask_cors import CORS
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = create_app(os.getenv("FLASK_CONFIG") or "default")
+
+
+migrate = Migrate(app, db, render_as_batch=False)
+
+# save time to set User as FindG.user when operating database commandshell
+@app.shell_context_processor
+def make_shell_context():
+    return dict(
+        db=db,
+    )
